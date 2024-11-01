58a7e23a174d8c9a19022aef451ae382a47c487f
KYM02 <gmyeongmin@gmail.com>
2024-11-01 18:06:08 +0900

    BCSD_Begginer_Week3_Assignment
    BCSD_Begginer_Week3_Assignment

---
+++ BCSD_WEEK3/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Bcsd_Week3_Assignment"
+include(":app")
+ 
