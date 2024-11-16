22f1468037bed48fc81661d141e8fc7291f9bb6b
KYM02 <gmyeongmin@gmail.com>
2024-11-16 15:42:52 +0900

    WEEK5
    Week5 Assignment

--- BCSD_WEEK3/app/src/main/java/com/example/bcsd_week3_assignment/MainActivity.kt
+++ BCSD_WEEK3/app/src/main/java/com/example/bcsd_week3_assignment/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) { // 앱이 최초 실행시
@@ -16,15 +17,27 @@ class MainActivity : AppCompatActivity() {
         btnToLoginLinear.setOnClickListener{toLoginLinear()}
         val btnToLoginRelative : Button = findViewById(R.id.btn_relative)
         btnToLoginRelative.setOnClickListener{toLoginRelative()}
+        val toast = Toast.makeText(this, "Hi", Toast.LENGTH_LONG) // this = MainActivity
+        //btnToLoginContraint.setOnLongClickListener{ toast.show() }
+        //btnToLoginContraint.text = "Hi" // 아래와 기능적 동일 다만 메소드 보다 직접적인 변수 변경이 선호
+        //btnToLoginContraint.setText("Hi")
     }
 
     fun toLoginContraint(){
-        startActivity(Intent(this, LoginActivityContraint::class.java))
+        //intent.putExtra(key, value) // intent 에 데이터 입력 int, string, char, bundle 등 다양한 정보 가능
+        //intent.getIntExtra(key) // intent 에 데이터 받아옴 - 내부에서 해당 값으로 Bundle 생성
+        //intent.getBundleExtra(key) // intent 에 Bundle 데이터 받아옴
+        // intent 와의 데이터 전달은 Bundle 을 이용하여 전달
+        // resultLauncher.launcher(intent) // intent 에 데이터 전송도 가능
+        startActivity(Intent(this, LoginActivityContraint::class.java)) // intent 로 인한 값 전달 불가
+        // startActivityForResult(intent, Code) ->
+        // resultLauncher - 되돌아올 때 데이터 전송(창이 닫혔을 때)
+        // setResult ? RESULT_OK / RESULT_CANCLED
     }
     fun toLoginLinear(){
         startActivity(Intent(this, LoginActivityLinear::class.java))
     }
-    fun toLoginRelative(){
+    fun toLoginRelative() {
         startActivity(Intent(this, LoginActivityRelative::class.java))
     }
 }
